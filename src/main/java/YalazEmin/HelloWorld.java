9f8cf6b9ba81f8511ed3d31561c8a7f1221789fb
emylz <43544835+emylz@users.noreply.github.com>
2018-10-02 18:46:00 +0200

    Add files via upload

---
+++ src/main/java/YalazEmin/HelloWorld.java
@@ -0,0 +1,13 @@
+package YalazEmin;
+
+import org.apache.log4j.Logger;
+
+public class HelloWorld {
+
+    private static final Logger LOGGER = Logger.getLogger(App.class);
+
+    public static void main(String[] args) {
+        LOGGER.error("Hello World !");
+
+    }
+}
